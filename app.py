615d63e28bfc6b5f62e8ed861b580a6550526eb6
rijusougata13 <rijusougata13@gmail.com>
2020-09-19 18:49:54 +0530

    added

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def hello():
 if __name__ == '__main__':
     app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=process.env.PORT ||5000)
+    app.run(host='0.0.0.0', port=process.env.PORT )
