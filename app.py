75960642a208653c7faa4d5642da25e3a3c10927
rijusougata13 <rijusougata13@gmail.com>
2020-09-19 18:06:02 +0530

    added

--- app.py
+++ app.py
@@ -3,43 +3,39 @@ from flask import jsonify
 from flask import Flask
 from flask import render_template
 from flask import request,redirect
-# import cv2
-# import tensorflow as tf
 from keras.models import load_model
-# import keras
-# from tensorflow import keras
-from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.applications.resnet50 import preprocess_input
 import os
 from keras.preprocessing import image
 import numpy as np
 
 
-categories=['Dog','Cat']
-
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join('static/images')
+
+
 @app.route('/',methods=['POST','GET'])
 def hello():
- 	
     if request.method=='POST':
-       
         file = request.files['file']
-        
         file_path=os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
         file.save(file_path)
-        img = image.load_img(file_path, target_size=(64, 64))
-        image1=img
+        img = image.load_img(file_path, target_size=(128, 128))
+        image_classifier = load_model('cat_dog_classifier_v1.h5')
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
-        model1 =load_model('dogcat_model_bak.h5')
-        prediction=model1.predict([x])
-        proba=model1.predict_proba([x])
-        #return(categories[int(prediction[0][0])])
-       
-        return render_template('submit.html',image=file_path,value=categories[int(prediction[0][0])])
+        class_labels = {0: 'Cat', 1: 'Dog'}
+        prediction = image_classifier.predict_classes(x)
+        percent_values = prediction.tolist()   
+        guess = class_labels[prediction[0][0]] 
+        print(percent_values)  
+        return render_template('submit.html',image=file_path,value=guess)
 
     else:
         return render_template("hello.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
